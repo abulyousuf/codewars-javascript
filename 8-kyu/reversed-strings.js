b15973bcde3b4c21c4e3f449016e501dd530d500
abulyousuf <abulfmyousuf@gmail.com>
2022-05-26 20:35:17 +0100

    Format code

--- 8-kyu/reversed-strings.js
+++ 8-kyu/reversed-strings.js
@@ -1,11 +1,11 @@
 /***
  * Complete the solution so that it reverses the string passed into it.
- * 
+ *
  * 'world'  =>  'dlrow'
  * 'word'   =>  'drow'
  */
 
-const solution = str => {
+const solution = (str) => {
   return str.split("").reverse().join("");
 };
 
