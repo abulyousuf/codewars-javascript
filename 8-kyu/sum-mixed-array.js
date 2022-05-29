42d822ee9e3c08a65ca30e20f8b6d1c3125cc0c7
abulyousuf <abulfmyousuf@gmail.com>
2022-05-29 22:49:07 +0100

    Solve kata - Sum Mixed Array

---
+++ 8-kyu/sum-mixed-array.js
@@ -0,0 +1,16 @@
+/***
+ * Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+ * Return your answer as a number.
+ */
+
+const sumMix = (x) => {
+  let sum = 0;
+
+  x.forEach((el) => (sum += Number(el)));
+
+  return sum;
+};
+
+console.log(sumMix([9, 3, "7", "3"])); // 22
+console.log(sumMix(["5", "0", 9, 3, 2, 1, "9", 6, 7])); // 42
+console.log(sumMix(["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"])); // 41
