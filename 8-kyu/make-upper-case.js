e5d88652026f5e0385a419fa20af9c5868aba51c
abulyousuf <abulfmyousuf@gmail.com>
2022-05-29 22:00:41 +0100

    Solve kata - MakeUpperCase

---
+++ 8-kyu/make-upper-case.js
@@ -0,0 +1,9 @@
+/***
+ * Write a function which converts the input string to uppercase.
+ */
+
+const makeUpperCase = (str) => {
+  return str.toUpperCase();
+};
+
+makeUpperCase("hello"); // "HELLO"
